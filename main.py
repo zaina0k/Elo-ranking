b4322c825088b26ede262baf474f888c2912cc47
Muhammad Altaf <muhammad.altaf-2@student.manchester.ac.uk>
2023-01-09 17:22:33 +0000

    calculating win potential from current ratings updating the ratings based off the result storing probability win in stage

--- main.py
+++ main.py
@@ -31,12 +31,30 @@ class Stage:
     def __init__(self):
         self.item1 = None
         self.item2 = None
+        self.item1ExScore = 0
+        self.item2ExScore = 0
     def setStage(self,comp1,comp2):
         self.item1 = comp1
         self.item2 = comp2
+        rat1 = self.item1.rating
+        rat2 = self.item2.rating
+        self.item1ExScore = 1/(1+10**((rat2-rat1)/400))
+        self.item2ExScore = 1/(1+10**((rat1-rat2)/400))
     def resetStage(self):
         self.item1 = None
         self.item2 = None
+        self.item1ExScore = 0
+        self.item2ExScore = 0
+    def stageItem1Winner(self):
+        if self.fullStage():
+            newRating = self.item1.rating + 32*(1-self.item1ExScore)
+            self.item1.updateStats(newRating)
+
+    def stageItem2Winner(self):
+        if self.fullstage():
+            newRating = self.item2.rating + 32*(1-self.item2ExScore)
+            self.item2.updateStats(newRating)
+
     def fullStage(self):
         if self.item1 == None or self.item2 == None:
             return False
@@ -46,7 +64,9 @@ class Stage:
         print_str = ""
         if self.fullStage():
             print_str += "First Item: " + self.item1.name + "\n"
+            print_str += "First Item win probability: " + str(round(self.item1ExScore,2))+ "\n"
             print_str += "Second Item: " + self.item2.name + "\n"
+            print_str += "Second Item win probability: " + str(round(self.item2ExScore,2)) + "\n"
         return print_str
 
 def changeFileName(originalname,newname,path,fileEnding):
@@ -67,14 +87,31 @@ def initialisePopulation():
     for i in range(len(filenames)):
         population.append(Item(filenames[i]))
 
+def item1Winner():
+    mainStage.stageItem1Winner()
+
+def item2Winner():
+    mainStage.stageItem2Winner()
+
 mainStage = Stage()
 
 item1 = Item("hello")
 item2 = Item("world")
+item1.rating= 1100
 
 mainStage.setStage(item1,item2)
 
+print(item1)
+print(item2)
+
 print(mainStage)
+
+item1Winner()
+
+print(item1)
+print(item2)
+
+
 
 
 

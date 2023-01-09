0d51898cc286002c9ef915ad6512535bb6313891
Muhammad Altaf <muhammad.altaf-2@student.manchester.ac.uk>
2023-01-09 16:52:25 +0000

    adding docstrings to classes. adding print function for stafe. testing stage holds item objects

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ filenames = next(walk(PATH), (None, None, []))[2]  # [] if no file
 population = []
 
 class Item:
+    """item class encapsulates each rated item carrying its current rating
+    as well as carrying its rating history as an 1d array"""
     def __init__(self,name):
         self.name = name
         self.rating = 1500
@@ -25,6 +27,7 @@ class Item:
         return(print_str)
 
 class Stage:
+    """stage class allows for 2 objects to be set up for comparison"""
     def __init__(self):
         self.item1 = None
         self.item2 = None
@@ -34,7 +37,17 @@ class Stage:
     def resetStage(self):
         self.item1 = None
         self.item2 = None
-
+    def fullStage(self):
+        if self.item1 == None or self.item2 == None:
+            return False
+        else:
+            return True
+    def __str__(self):
+        print_str = ""
+        if self.fullStage():
+            print_str += "First Item: " + self.item1.name + "\n"
+            print_str += "Second Item: " + self.item2.name + "\n"
+        return print_str
 
 def changeFileName(originalname,newname,path,fileEnding):
     filenames = next(walk(path), (None, None, []))[2]  # [] if no file
@@ -54,7 +67,14 @@ def initialisePopulation():
     for i in range(len(filenames)):
         population.append(Item(filenames[i]))
 
+mainStage = Stage()
 
+item1 = Item("hello")
+item2 = Item("world")
+
+mainStage.setStage(item1,item2)
+
+print(mainStage)
 
 
 

8459d1c2dff0c50e4e13549dcb58789902fe0bc3
Muhammad Altaf <muhammad.altaf-2@student.manchester.ac.uk>
2023-01-11 02:04:08 +0000

    new page created in gui for dev operations simulating now easier with validation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from guizero import App,PushButton,Text,Box,Picture
+from guizero import App,PushButton,Text,Box,Picture,TextBox
 from os import walk,rename
 from random import shuffle
 import matplotlib. pyplot as plt
@@ -186,23 +186,34 @@ def displayTop5Ratings():
     top5_ratings_box.show()
 
 def plotHistory(object=None):
+    ###needs to be changed
     simulateRankedPlay(100)
     for item in population:
         print(item)
-
     mx = 0
     for item in population:
         if len(item.ratingHistory)>mx:
             mx = len(item.ratingHistory)
             object = item
-
-
+    #######################
     if object==None:
         return None
     else:
         rankHistory = object.ratingHistory
         plt.plot(rankHistory)
         plt.show()
+
+def dev_simulate_button():
+    user_input = dev_sim_input.value
+    if user_input.isdigit():
+        if int(user_input) <= 100:
+            print("simulating " + str(user_input) + " times")
+            simulateRankedPlay(int(user_input))
+        else:
+            print("only simulate 100 or less at a time")
+    else:
+        print("enter a number into the text box")
+
 
 #GUI changing page function
 def home_to_settings():
@@ -236,6 +247,14 @@ def rank_history_to_settings():
     rank_history_box.hide()
     settings_box.show() 
 
+def settings_to_dev():
+    settings_box.hide()
+    dev_box.show()
+
+def dev_to_settings():
+    dev_box.hide()
+    settings_box.show()
+
 ##START##
 initialisePopulation()
 mainStage = Stage()
@@ -244,7 +263,7 @@ mainStage.setStage(initialItems[0],initialItems[1])
 
 
 #Home Page
-app = App(title="Elo ranking",bg="light grey",height=1000,width=1500)
+app = App(title="Elo ranking",bg="dark grey",height=1000,width=1500)
 home_box = Box(master=app,layout="grid",visible=True)
 home_title = Text(master=home_box,text="Welcome to Elo Ranking",grid=[5,0],size=30,font="courier new")
 home_start_btn = PushButton(master=home_box,text="Start",grid=[10,10],padx=10,pady=10,command=home_to_comp)
@@ -254,9 +273,10 @@ home_quit_btn = PushButton(master=home_box,text="Quit",grid=[0,10],command=quit_
 #Settings page
 settings_box = Box(master=app,layout="grid",visible=False)
 settings_title = Text(master=settings_box,text="Settings",grid=[5,0],size=30,font="courier new")
-settings_home_btn = PushButton(master=settings_box,text="Home",grid=[0,10],command=settings_to_home,padx=10,pady=10)
-settings_top5_btn = PushButton(master=settings_box,text="Display top 5 rated",grid=[5,5],command=displayTop5Ratings,padx=10,pady=10)
-settings_rank_history_btn = PushButton(master=settings_box,text="Individual item rank history",grid=[6,5],command=settings_to_rank_history,padx=10,pady=10)
+settings_home_btn = PushButton(master=settings_box,text="Home",grid=[4,6],command=settings_to_home,padx=10,pady=10)
+settings_top5_btn = PushButton(master=settings_box,text="Display top 5 rated",grid=[6,5],command=displayTop5Ratings,padx=10,pady=10,align="left")
+settings_rank_history_btn = PushButton(master=settings_box,text="Individual item rank history",grid=[6,6],command=settings_to_rank_history,padx=10,pady=10,align="left")
+settings_dev_btn = PushButton(master=settings_box,text="Dev",grid=[4,5],command=settings_to_dev,padx=10,pady=10)
 
 #Top 5 ratings page
 top5_ratings_box = Box(master=app,layout="grid",visible=False)
@@ -275,13 +295,20 @@ rank_history_title = Text(master=rank_history_box,text="Rank history",grid=[0,0]
 rank_his_to_settings = PushButton(master=rank_history_box,text="Back",grid=[0,10],command=rank_history_to_settings,padx=10,pady=10)
 rank_his_show_btn = PushButton(master=rank_history_box,text="Show",grid=[5,5],command=plotHistory,padx=10,pady=10)
 
+#Developer page
+dev_box = Box(master=app,layout="grid",visible=False)
+dev_title = Text(master=dev_box,text="Dev options",grid=[0,0],size=30,font="courier new",align="left")
+dev_simulate = PushButton(master=dev_box,text="Simulate",grid=[1,5],command=dev_simulate_button,padx=10,pady=10)###
+dev_sim_input = TextBox(master=dev_box,grid=[0,5])
+dev_to_settings_btn = PushButton(master=dev_box,text="Back",grid=[0,10],command=dev_to_settings,padx=10,pady=10)
+
 #Comparing items page
 comp_box = Box(master=app,layout="grid",visible=False)
 comp_home_button = PushButton(master=comp_box,text="Home",grid=[0,10],command=comp_to_home,padx=10,pady=10)
-comp_title = Text(master=settings_box,text="Competitive",grid=[5,0],size=30,font="courier new")
+comp_title = Text(master=comp_box,text="Competitive",grid=[1,0],size=30,font="courier new")
 picture1 = Picture(master=comp_box,grid=[2,2],image=PATH+mainStage.item1.name,width=480,height=854)
 picture2 = Picture(master=comp_box,grid=[4,2],image=PATH+mainStage.item2.name,width=480,height=854)
-comp1_btn = PushButton(master=comp_box,text="OPTION 1",grid=[1,2],command=item1Winner,padx=10,pady=10)
+comp1_btn = PushButton(master=comp_box,text="OPTION 1",grid=[1,2],command=item1Winner,padx=10,pady=10,align="right")
 comp2_btn = PushButton(master=comp_box,text="OPTION 2",grid=[5,2],command=item2Winner,padx=10,pady=10)
 
 app.display()
